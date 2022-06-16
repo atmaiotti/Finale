4bc30529a421b6f20dd979532c57963cd5ef4341
atmaiotti <atmaiotti@hotmailcom>
2022-06-16 18:24:07 +0300

    add video?

---
+++ Finale/VideoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  VideoViewController.swift
+//  Finale
+//
+//  Created by Adriana Maiotti on 6/16/22.
+//
+
+import UIKit
+import WebKit
+
+class VideoViewController: UIViewController {
+
+    @IBOutlet weak var MyWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let googleURL = URL(string: "https://www.youtube.com/watch?v=Bhi7S06pwv4")
+        MyWebView.load(URLRequest(url:googleURL!))
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
